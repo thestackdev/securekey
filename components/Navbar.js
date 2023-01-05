20594f1b55aefbc7ca8ade7437d497fb8a6ec72e
thestackdev <thestackdev@fullstacklab.org>
2023-01-05 19:57:25 +0530

    Updates

--- components/Navbar.js
+++ components/Navbar.js
@@ -10,9 +10,7 @@ export default function () {
       </Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse>
-        <Navbar.Link href="/" active={true}>
-          Home
-        </Navbar.Link>
+        <Navbar.Link href="#">Home</Navbar.Link>
         <Navbar.Link href="#">About</Navbar.Link>
         <Navbar.Link href="#">Contact</Navbar.Link>
       </Navbar.Collapse>
